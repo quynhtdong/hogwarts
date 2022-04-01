835ac9b13d2a5dcc2b6bdcfc257e0ec3f504278c
Quynh <quynhdongikn@gmail.com>
2022-04-01 18:32:44 -0500

    git initial commit

---
+++ src/main/java/edu/tcu/cs/hogwartartifactsonline/HogwartartifactsonlineApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwartartifactsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartartifactsonlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartartifactsonlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tcu/cs/hogwartartifactsonline/HogwartartifactsonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwartartifactsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartartifactsonlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
